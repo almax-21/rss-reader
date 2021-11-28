b42ab35685972dcf67c986c2c8b6ede4f1e776f0
almax-21 <almaev21.max@gmail.com>
2021-11-28 22:27:15 +0400

    feat: additional error messages

--- src/store/async-actions/getRSSData.ts
+++ src/store/async-actions/getRSSData.ts
@@ -15,17 +15,19 @@ interface AsyncFeedActionData {
 	intl: IntlShape<string | ReactNode>;
 }
 
+const TIMEOUT_MS = 30000; 
+
 export const getRSSData = createAsyncThunk(
 	'rss/getRSSData',
 	async ({ feedUrl, intl }: AsyncFeedActionData, thunkAPI) => {
 		try {
 			thunkAPI.dispatch(showLoading());
 
-			const { data } = await axios.get(
-				`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(
-					feedUrl
-				)}`
-			);
+			const proxedUrl = `https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(
+				feedUrl
+			)}`;
+
+			const { data } = await axios.get(proxedUrl, { timeout: TIMEOUT_MS });
 
 			const serializedContent: XMLHttpRequestResponseType = data.contents;
 			const { parsedFeed, parsedPosts }: ParsedRSS = parseRSS(
@@ -50,9 +52,24 @@ export const getRSSData = createAsyncThunk(
 
 			return rssData;
 		} catch (err) {
+			const message = (err as Error).message;
+			const isTimeoutError = (/^timeout.*exceeded$/).test(message);
+
+			if (isTimeoutError) {
+				return thunkAPI.rejectWithValue(
+					intl.formatMessage({ id: MESSAGES.ERROR_TIMEOUT })
+				);
+			}
+
+			if (message === MESSAGES.ERROR_INCORRECT_RSS) {
+				return thunkAPI.rejectWithValue(
+					intl.formatMessage({ id: MESSAGES.ERROR_INCORRECT_RSS })
+				);
+			}
+
 			return navigator.onLine
 				? thunkAPI.rejectWithValue(
-						intl.formatMessage({ id: MESSAGES.ERROR_INCORRECT_RSS })
+						intl.formatMessage({ id: MESSAGES.ERROR_UNKNOWN })
 				  )
 				: thunkAPI.rejectWithValue(
 						intl.formatMessage({ id: MESSAGES.ERROR_NETWORK })
