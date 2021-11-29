a481f48a76dd6b7bffb1c60b2e385ca847897921
almax-21 <almaev21.max@gmail.com>
2021-11-29 13:46:04 +0400

    build: disable workbox logging

--- src/sw.ts
+++ src/sw.ts
@@ -3,6 +3,8 @@ import { precacheAndRoute } from 'workbox-precaching';
 
 declare const self: ServiceWorkerGlobalScope;
 
+self.__WB_DISABLE_DEV_LOGS = true;
+
 clientsClaim();
 
 self.skipWaiting();
