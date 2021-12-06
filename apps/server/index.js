7530ecb7d61c33f82c7ec74d242c13f043bd8544
almax-21 <almaev21.max@gmail.com>
2021-12-06 13:48:34 +0400

    build(server): hide db uri

--- apps/server/index.js
+++ apps/server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
+const dotenv = require('dotenv');
 
 const cors = require('cors');
 const userRouter = require('./router/user.routes');
@@ -11,11 +12,14 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/user', userRouter);
 
+dotenv.config();
+
 const PORT = process.env.PORT || config.get('serverPort');
+const dbURI = process.env.DB_URI;
 
 const start = async () => {
 	try {
-		await mongoose.connect(config.get('dbUrl'));
+		await mongoose.connect(dbURI);
 
 		app.listen(PORT, () => {
 			console.log(`Server started on port ${PORT}`);
