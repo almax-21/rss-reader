60738b60be2234e81ec313652d59a4b8acc3e8b3
almax-21 <almaev21.max@gmail.com>
2021-12-08 17:17:36 +0400

    perf: search post filter

--- apps/client/src/components/posts/PostFilter/index.tsx
+++ apps/client/src/components/posts/PostFilter/index.tsx
@@ -1,5 +1,12 @@
-import React, { FC, useState } from 'react';
-import { Badge, Button, Form, InputGroup } from 'react-bootstrap';
+import React, {
+	ChangeEvent,
+	FC,
+	FormEvent,
+	useEffect,
+	useRef,
+	useState,
+} from 'react';
+import { Button, Form, InputGroup } from 'react-bootstrap';
 import { FormattedMessage, useIntl } from 'react-intl';
 
 import useTypedDispatch from '../../../hooks/redux/useTypedDispatch';
@@ -16,6 +23,7 @@ import {
 } from '../../../store/slices/postsSlice';
 import { POST_STATES, POST_TYPE } from '../../../store/types';
 import { IPostFilter } from '../../../types';
+import { debounce } from '../../../utils/perfomance';
 import MyDropDown from '../../UI/MyDropDown';
 import MyModal from '../../UI/MyModal/index';
 import { MODAL_TYPES } from '../../UI/MyModal/types';
@@ -28,7 +36,6 @@ interface PostFilterProps {
 }
 
 const PostFilter: FC<PostFilterProps> = ({ postFilter, resetActivePage }) => {
-	const [searchQuery, setSearchQuery] = useState<string>('');
 	const [isShowModal, setIsShowModal] = useState<boolean>(false);
 
 	const filterValues = Object.values(POST_STATES);
@@ -37,25 +44,26 @@ const PostFilter: FC<PostFilterProps> = ({ postFilter, resetActivePage }) => {
 	const unreadPostsCount = useTypedSelector(selectUnreadPostsCount);
 
 	const dispatch = useTypedDispatch();
-
 	const intl = useIntl();
 
-	const handleChangeQuery = (evt: React.ChangeEvent<HTMLInputElement>) => {
-		setSearchQuery(evt.target.value);
-	};
+	const formRef = useRef<HTMLFormElement | null>(null);
 
-	const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+	useEffect(() => {
+		formRef.current?.reset();
+	}, [activeFeedId]);
+
+	const handleSearch = debounce((evt: ChangeEvent<HTMLInputElement>) => {
+		const { value } = evt.target;
+
+		dispatch(updateFilterQuery(value));
+	}, 300);
+
+	const handleReset = (evt: FormEvent<HTMLFormElement>) => {
 		evt.preventDefault();
 
-		dispatch(updateFilterQuery(searchQuery));
+		formRef.current?.reset();
 
-		setSearchQuery('');
-		resetActivePage();
-	};
-
-	const handleReset = () => {
 		dispatch(updateFilterQuery(''));
-		setSearchQuery('');
 		resetActivePage();
 	};
 
@@ -85,58 +93,44 @@ const PostFilter: FC<PostFilterProps> = ({ postFilter, resetActivePage }) => {
 	return (
 		<>
 			<div className="filter">
-				<Form onSubmit={handleSubmit}>
+				<Form ref={formRef} onSubmit={handleReset}>
 					<InputGroup className="filter__group">
 						<Form.Control
 							aria-label={intl.formatMessage({ id: MESSAGES.SEARCH })}
 							className="filter__input"
 							placeholder={intl.formatMessage({ id: MESSAGES.SEARCH }) + '...'}
 							type="text"
-							value={searchQuery}
-							onChange={handleChangeQuery}
+							onChange={handleSearch}
 						/>
-						{searchQuery ? (
-							<Button className="filter__btn" type="submit" variant="primary">
-								<FormattedMessage id={MESSAGES.FIND} />
-							</Button>
-						) : (
-							<Button
-								className="filter__btn"
-								variant="outline-info"
-								onClick={handleReset}
-							>
-								<FormattedMessage id={MESSAGES.RESET} />
-							</Button>
-						)}
+						<Button
+							className="filter__btn"
+							type="submit"
+							variant="outline-info"
+						>
+							<FormattedMessage id={MESSAGES.RESET} />
+						</Button>
 					</InputGroup>
 				</Form>
 
-				<div className="d-flex flex-wrap-reverse">
-					<div className="d-flex flex-wrap justify-content-between flex-grow-1">
-						<MyDropDown
-							activeValue={postFilter.state}
-							handleSetActiveValue={handleSwitchFilterState}
-							title={intl.formatMessage({ id: postFilter.state })}
-							values={filterValues}
-							variant="outline-secondary"
-						/>
-						<Button
-							className="filter__btn filter__btn--mark-all"
-							disabled={unreadPostsCount === 0}
-							variant="outline-danger"
-							onClick={handleOpenModal}
-						>
-							<FormattedMessage id={MESSAGES.MARK_ALL_AS_READ} />
-						</Button>
-					</div>
-					{postFilter.query && (
-						<Badge pill bg="light" text="dark">
-							<FormattedMessage id={MESSAGES.SEARCH_BY_REQUEST} />:{' '}
-							{postFilter.query}
-						</Badge>
-					)}
+				<div className="d-flex flex-wrap justify-content-between flex-grow-1">
+					<MyDropDown
+						activeValue={postFilter.state}
+						handleSetActiveValue={handleSwitchFilterState}
+						title={intl.formatMessage({ id: postFilter.state })}
+						values={filterValues}
+						variant="outline-secondary"
+					/>
+					<Button
+						className="filter__btn filter__btn--mark-all"
+						disabled={unreadPostsCount === 0}
+						variant="outline-danger"
+						onClick={handleOpenModal}
+					>
+						<FormattedMessage id={MESSAGES.MARK_ALL_AS_READ} />
+					</Button>
 				</div>
 			</div>
+
 			<MyModal
 				description={intl.formatMessage({ id: MESSAGES.MARK_ALL_READ_WARNING })}
 				handleAction={handleMarkAllRead(activeFeedId)}
--- apps/client/src/utils/perfomance.ts
+++ apps/client/src/utils/perfomance.ts
@@ -1,17 +1,31 @@
+import { TimeoutId } from '../types';
+
 export const throttle = (cb: (...args: any[]) => any, delay: number) => {
 	let isThrottle = false;
 
-	return () => {
+	return (...args: any[]) => {
 		if (isThrottle) {
 			return;
 		}
 
 		isThrottle = true;
 
-		cb();
+		cb(...args);
 
 		setTimeout(() => {
 			isThrottle = false;
 		}, delay);
+	};
+};
+
+export const debounce = (cb: (...args: any[]) => any, delay: number) => {
+	let timerId = -1 as unknown as TimeoutId;
+
+	return (...args: any[]) => {
+		clearTimeout(timerId);
+
+		const currentId = setTimeout(() => cb(...args), delay);
+
+		timerId = currentId;
 	};
 };
