2dac579ec4f4135883980b48fcbf529cdca9e999
almax-21 <almaev21.max@gmail.com>
2021-12-12 19:43:35 +0400

    style(client): update content skeleton

--- apps/client/src/components/UI/ContentSkeleton/index.tsx
+++ apps/client/src/components/UI/ContentSkeleton/index.tsx
@@ -7,30 +7,60 @@ const ContentSkeleton: FC = () => {
 	return (
 		<div className="content-skeleton">
 			<div className="content-skeleton__item">
-				<Placeholder animation="glow" as="p">
-					<Placeholder xs={12} />
+				<Placeholder animation="wave" as="p" className="mb-3">
+					<Placeholder xs={2} />
 				</Placeholder>
-				<Placeholder animation="wave" as="p">
-					<Placeholder xs={12} />
+				<Placeholder animation="glow" as="h2" className="mb-4">
+					<Placeholder xs={8} />
 				</Placeholder>
-				<Placeholder animation="glow" as="p">
-					<Placeholder xs={12} />
+				<Placeholder
+					animation="wave"
+					as="p"
+					className="d-flex justify-content-between mb-4"
+					xs={8}
+				>
+					<Placeholder.Button animation="wave" xs={3} />
+					<Placeholder.Button animation="wave" xs={3} />
 				</Placeholder>
-				<Placeholder animation="wave" as="p">
-					<Placeholder xs={12} />
+				<Placeholder
+					animation="glow"
+					as="p"
+					className="d-flex justify-content-between mb-5"
+					xs={12}
+				>
+					<Placeholder xs={10} />
+					<Placeholder.Button animation="glow" xs={1} />
+				</Placeholder>
+				<Placeholder
+					animation="wave"
+					as="p"
+					className="d-flex justify-content-between mb-5"
+					xs={12}
+				>
+					<Placeholder xs={10} />
+					<Placeholder.Button animation="wave" xs={1} />
+				</Placeholder>
+				<Placeholder
+					animation="glow"
+					as="p"
+					className="d-flex justify-content-between mb-5"
+					xs={12}
+				>
+					<Placeholder xs={10} />
+					<Placeholder.Button animation="glow" xs={1} />
 				</Placeholder>
 			</div>
 			<div className="content-skeleton__item">
-				<Placeholder animation="glow" as="p">
+				<Placeholder animation="wave" as="p" className="mb-3">
+					<Placeholder xs={2} />
+				</Placeholder>
+				<Placeholder animation="glow" as="h1" className="mb-3">
+					<Placeholder bg="primary" xs={12} />
+				</Placeholder>
+				<Placeholder animation="wave" as="h1" className="mb-3">
 					<Placeholder xs={12} />
 				</Placeholder>
-				<Placeholder animation="wave" as="p">
-					<Placeholder xs={12} />
-				</Placeholder>
-				<Placeholder animation="glow" as="p">
-					<Placeholder xs={12} />
-				</Placeholder>
-				<Placeholder animation="wave" as="p">
+				<Placeholder animation="glow" as="h1" className="mb-3">
 					<Placeholder xs={12} />
 				</Placeholder>
 			</div>
