f52bd2a6bca284391613106f28e2dbad601eacc9
almax-21 <almaev21.max@gmail.com>
2021-12-13 04:53:31 +0400

    chore(api): increase max size payload

--- apps/api/index.js
+++ apps/api/index.js
@@ -11,7 +11,7 @@ const postRouter = require('./router/post.routes');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 app.use('/user', userRouter);
 app.use('/feeds', feedRouter);
