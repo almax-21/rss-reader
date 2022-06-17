08b02ec4a66a47a36e9867b03297fe1e54571ea9
almax-21 <almaev21.max@gmail.com>
2022-06-17 23:15:59 +0400

    fix: remove unnecessary fonts copying by webpack

--- apps/client/webpack.config.js
+++ apps/client/webpack.config.js
@@ -60,7 +60,6 @@ module.exports = {
 		new CopyPlugin({
 			patterns: [
 				{ from: 'assets/icons', to: 'icons/' },
-				{ from: 'assets/fonts', to: 'fonts/' },
 				{ from: 'manifest.webmanifest' },
 			],
 		}),
